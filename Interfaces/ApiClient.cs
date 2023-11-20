0c9c8c32e70cb4f07772d786aa2497261079f782
Agustín Amaya <80552327+bluehat8@users.noreply.github.com>
2023-11-19 18:52:47 -0600

    Correcciones en el api Client. Las excepciones interrumpen el sistema

--- Interfaces/ApiClient.cs
+++ Interfaces/ApiClient.cs
@@ -15,107 +15,47 @@ namespace DentalApp.Services
 
         public async Task<TResponse> GetAsync<TResponse>(string url)
         {
-            try
-            {
-                var response = await _httpClient.GetAsync(url);
-                response.EnsureSuccessStatusCode();
-                var jsonResponse = await response.Content.ReadAsStringAsync();
-                var result = JsonConvert.DeserializeObject<TResponse>(jsonResponse);
-                return result;
-            }
-            catch (HttpRequestException ex)
-            {
-                throw new ApiException("Error al realizar la solicitud HTTP", ex);
-            }
-            catch (JsonException ex)
-            {
-                throw new ApiException("Error al deserializar la respuesta JSON", ex);
-            }
-            catch (Exception ex)
-            {
-                throw new ApiException("Error inesperado", ex);
-            }
+            var response = await _httpClient.GetAsync(url);
+            var jsonResponse = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<TResponse>(jsonResponse);
+            return result;
         }
 
         public async Task<TResponse> PutAsync<TRequest, TResponse>(string url, TRequest request)
         {
-            try
-            {
-                var json = JsonConvert.SerializeObject(request);
-                var content = new StringContent(json, Encoding.UTF8, "application/json");
-                var response = await _httpClient.PutAsync(url, content);
-                response.EnsureSuccessStatusCode();
-                var jsonResponse = await response.Content.ReadAsStringAsync();
-                var result = JsonConvert.DeserializeObject<TResponse>(jsonResponse);
-                return result;
-            }
-            catch (HttpRequestException ex)
-            {
-                throw new ApiException("Error al realizar la solicitud HTTP", ex);
-            }
-            catch (JsonException ex)
-            {
-                throw new ApiException("Error al deserializar la respuesta JSON", ex);
-            }
-            catch (Exception ex)
-            {
-                throw new ApiException("Error inesperado", ex);
-            }
+            var json = JsonConvert.SerializeObject(request);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            var response = await _httpClient.PutAsync(url, content);
+            var jsonResponse = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<TResponse>(jsonResponse);
+            return result;
         }
 
         public async Task<TResponse> DeleteAsync<TResponse>(string url)
         {
-            try
-            {
-                var response = await _httpClient.DeleteAsync(url);
-                response.EnsureSuccessStatusCode();
-                var jsonResponse = await response.Content.ReadAsStringAsync();
-                var result = JsonConvert.DeserializeObject<TResponse>(jsonResponse);
-                return result;
-            }
-            catch (HttpRequestException ex)
-            {
-                throw new ApiException("Error al realizar la solicitud HTTP", ex);
-            }
-            catch (JsonException ex)
-            {
-                throw new ApiException("Error al deserializar la respuesta JSON", ex);
-            }
-            catch (Exception ex)
-            {
-                throw new ApiException("Error inesperado", ex);
-            }
+            var response = await _httpClient.DeleteAsync(url);
+            var jsonResponse = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<TResponse>(jsonResponse);
+            return result;
         }
 
         public async Task<TResponse> PostAsync<TRequest, TResponse>(string url, TRequest request)
         {
-            try
-            {
-                var json = JsonConvert.SerializeObject(request);
-                var content = new StringContent(json, Encoding.UTF8, "application/json");
-                var response = await _httpClient.PostAsync(url, content);
-                response.EnsureSuccessStatusCode();
-                var jsonResponse = await response.Content.ReadAsStringAsync();
-                var result = JsonConvert.DeserializeObject<TResponse>(jsonResponse);
-                return result;
-            }
-            catch (HttpRequestException ex)
-            {
-                throw new ApiException("Error al realizar la solicitud HTTP", ex);
-            }
-            catch (JsonException ex)
-            {
-                throw new ApiException("Error al deserializar la respuesta JSON", ex);
-            }
-            catch (Exception ex)
-            {
-                throw new ApiException("Error inesperado", ex);
-            }
+            var json = JsonConvert.SerializeObject(request);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            var response = await _httpClient.PostAsync(url, content);
+            var jsonResponse = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<TResponse>(jsonResponse);
+            return result;
         }
     }
 
     public class ApiException : Exception
     {
+        public ApiException(string? message) : base(message)
+        {
+        }
+
         public ApiException(string message, Exception innerException) : base(message, innerException)
         {
         }
